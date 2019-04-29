97fa5832c410feee15b27c65c52b4751d17008a1
jian zhang <1278151178@qq.com>
2019-04-29 21:11:13 +0800

    success corner color

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 450 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
@@ -20,7 +21,7 @@ const char *vertexShaderSource = "#version 450 core\n"
     "}\0";
 const char *fragmentShaderSource = "#version 450 core\n"
     "out vec4 FragColor;\n"
-    "int vec3 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   FragColor = vec4(ourColor, 1.0f);\n"
